bee0dc66764cc5dba40bd92dfddd4e3718e1c275
Jeremy Brooks <jeremy@platform9.com>
2016-06-24 17:12:31 -0700

    packaged commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='pf9watcher',
+      version='0.0.1',
+      description='Host monitoring and evacuation',
+      url='',
+      author='Jeremy Brooks',
+      author_email='jeremy@platform9.com',
+      license='MIT',
+      packages=['pf9watcher'],
+      install_requires=[
+          'pycrypto',
+          'python-keystoneclient',
+          'python-novaclient',
+      ],      
+      zip_safe=False)
