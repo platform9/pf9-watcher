509069b4669efa91e975fa653c57c91fd52b9afd
Blake Covarrubias <blake@platform9.com>
2017-01-23 13:41:10 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pf9watcher',
-      version='0.0.1',
+      version='0.0.2',
       description='Host monitoring and evacuation',
       url='http://github.com/platform9/pf9-watcher',
       author='Jeremy Brooks',
